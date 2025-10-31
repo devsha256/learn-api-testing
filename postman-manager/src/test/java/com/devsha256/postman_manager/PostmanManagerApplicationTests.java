cafe3297c37b5875116f4e692dd0bcecdc6c70cd
devsha256 <dev.hsaddam@gmail.com>
2025-10-31 15:49:25 +0530

    Add postman manager springboot app

---
+++ postman-manager/src/test/java/com/devsha256/postman_manager/PostmanManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsha256.postman_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmanManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
